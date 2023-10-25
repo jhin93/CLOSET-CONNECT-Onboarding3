aca4ad63658285b762ed4546bd6f27596f708ad4
jhin93 <scene1993@gmail.com>
2023-10-25 17:44:18 +0900

    따로 생성해서 연습

--- onboarding3-jhin/src/redux/constants/action-types.ts
+++
@@ -1,9 +0,0 @@
-
-export const ADD_ARTICLE = "ADD_ARTICLE";
-
-
-
-
-
-
-
--- onboarding3-jhin/src/redux/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-
-const initialState = {
-    articles: []
-};
-
-const rootReducer = (state = initialState, action) => state;
-
-export default rootReducer;
